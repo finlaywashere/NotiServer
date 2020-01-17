3d289df39bb226d2e004ecb90170f23b3d9d2fcc
Finlay Maroney <finman292004@protonmail.com>
2020-01-17 15:40:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,3 +13,4 @@ cp notiserver.service /usr/lib/systemd/system/
 mkdir -p /etc/notiserver/
 
 echo "Installed Notiserver! To use it start/enable notiserver.service!"
+echo "Also note that you must put your certificate and key for SSL in /etc/notiserver/ as cert.pem and key.pem respectively"
